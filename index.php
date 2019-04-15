c9763df940aad63d63547f2d9f7fba9220f3707c
amrise <syamsulamrijamaludin@gmail.com>
2019-04-15 23:38:48 +0800

    update index page

--- index.php
+++ index.php
@@ -1,50 +1,421 @@
-<?php 
-require ('database/custlogin.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="favicon.ico">
+		<title>UTeM e-Printing</title>
+		<!-- Bootstrap core CSS -->
+		<link href="cssNantes/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<!-- Custom styles for this template -->
+		<link href="cssNantes/owl.carousel.css" rel="stylesheet">
+		<link href="cssNantes/owl.theme.default.min.css"  rel="stylesheet">
+		<link href="cssNantes/animate.css" rel="stylesheet">
+		<link href="cssNantes/style.css" rel="stylesheet">
+	</head>
+	<body id="page-top">
+		<!-- Navigation -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header page-scroll">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand page-scroll" href="#page-top"><img src="imagesNantes/logo.png" alt="Sanza theme logo"></a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="hidden">
+							<a href="#page-top"></a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#page-top">Home</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#about">About</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#features">Features</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#portfolio">Portfolio</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#contact">Contact</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#partners">Login</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#team">Register</a>
+						</li>
+					</ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
+		
 
-<head>
+		
+		<!-- ABOUT -->
+		<section id="about" class="light-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>About</h2>
+							<p>UTeM e-Printing enable the customer in printing their task with easy, fast and everywhere they go. <br><strong>Just create your account. Make a your first transaction and you are on a right away!!</strong></p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<!-- about module -->
+					<div class="col-md-3 text-center">
+						<div class="mz-module">
+							<div class="mz-module-about">
+								<h3>Print 100+ pages</h3>
+								<p><strong>RM 0.10</strong> per page for black and colour.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end about module -->
+					<!-- about module -->
+					<div class="col-md-3 text-center">
+						<div class="mz-module">
+							<div class="mz-module-about">
+								<h3>Black Print</h3>
+								<p><strong>RM 0.10</strong> per page.</p><br>
+								<h3>Colour Print</h3>
+								<p><strong>RM 0.50</strong> per page.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end about module -->
+					<!-- about module -->
+					<div class="col-md-3 text-center">
+						<div class="mz-module">
+							<div class="mz-module-about">
+								<h3>Binding Comb</h3>
+								<p><strong>RM 1.00</strong> per comb.</p><br>
+								<h3>Binding Tape</h3>
+								<p><strong>RM 0.50</strong> per tape.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end about module -->
+					<!-- about module -->
+					<div class="col-md-3 text-center">
+						<div class="mz-module">
+							<div class="mz-module-about">
+								<h3>Transparent Cover</h3>
+								<p><strong>RM 0.50</strong> per item.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end about module -->
+				</div>
+			</div>
+			<!-- /.container -->
+		</section>
 
-<title>E-Printing</title>
+		<!-- FEATURES -->
+		<section id="features" class="section-features">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>Features</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row row-gutter">
+					<div class="col-md-4 col-gutter">
+						<div class="featured-item">
+							<div class="featured-icon"><i class="fa fa-television"></i></div>
+							<div class="featured-text">
+								<h4>Booking Online</h4>
+								<p>Purchase a printing service through Laptop screen, Mobile or Tablet at everywhere.</p>
+							</div>
+						</div>
+						<div class="featured-item">
+							<div class="featured-icon"><i class="fa fa-qrcode"></i></div>
+							<div class="featured-text">
+								<h4>Qrcode transaction</h4>
+								<p>You can use your given Qrcode to take your printing purchased order.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-gutter">
+						<div class="featured-item">
+							<div class="featured-icon"><i class="fa fa-dollar"></i></div>
+							<div class="featured-text">
+								<h4>Online Payment</h4>
+								<p>Make a payment online through Paypal to pay printing purchased.</p>
+							</div>
+						</div>
+						<div class="featured-item">
+							<div class="featured-icon"><i class="fa fa-print"></i></div>
+							<div class="featured-text">
+								<h4>Fast printing process </h4>
+								<p>It may takes 2 or 3 days to print your order depend on the amount purchased.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-gutter">
+						<div class="featured-item">
+							<div class="featured-icon"><i class="fa fa-bars"></i></div>
+							<div class="featured-text">
+								<h4>Record Purchased</h4>
+								<p>Status of printing booking order can view either the printing still in process or done printing.</p>
+							</div>
+						</div>
+						<div class="featured-item">
+							<div class="featured-icon"><i class="fa fa-envelope-o"></i></div>
+							<div class="featured-text">
+								<h4>Give remainder</h4>
+								<p>We will notify you by email when the printing is done.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
-<link href="css/login.css" rel="stylesheet">
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet">
+		<!-- PORTFOLIO -->
+		<section id="portfolio" class="light-bg">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<div class="section-title">
+						<h2>Portfolio</h2>
+						<p>Our business printing portfolio is growing in a couple of months since JANUARY 2019.</p>
+					</div>
+				</div>
+			</div>
+			</div><!-- end container -->
+		</section>
 
-</head>
+		<!-- <section class="dark-bg short-section stats-bar">
+			<div class="container text-center">
+				<div class="row">
+					
+					<div class="col-md-4 mb-sm-30">
+						<div class="counter-item">
+							<h2 class="stat-number" data-n="167">0</h2>
+							<h6>Customer</h6>
+						</div>
+					</div>
+					<div class="col-md-4 mb-sm-30">
+						<div class="counter-item">
+							<h2 class="stat-number" data-n="6">0</h2>
+							<h6>Purchased Made</h6>
+						</div>
+					</div>
+					<div class="col-md-4 mb-sm-30">
+						<div class="counter-item">
+							<h2 class="stat-number" data-n="34">0</h2>
+							<h6>Purchased Delivered</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> -->
 
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card card-signin my-5">
-          <div class="card-body">
-            <h5 class="card-title text-center">Sign In</h5>
-            <form class="form-signin" method="post">
+		
 
-               <div class="form-label-group">
-                <input type="text" id="username" class="form-control" name="username" placeholder="Username" required autofocus>
-                <label for="username">Username</label>
-              </div>
+		<section id="contact" class="dark-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>Contact Us</h2>
+							<p>If you have some Questions or need Help! Please Contact Us!<br>We respond you as soon as possible.</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<div class="section-text">
+							<h4>Our Business Office</h4>
+							<p>No. 52, Jalan TU 42, 75450 Ayer Keroh, Melaka</p>
+							<p><i class="fa fa-phone"></i> +60 12-345-6789</p>
+							<p><i class="fa fa-envelope"></i> utemprintpsm@gmail.com</p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="section-text">
+							<h4>Business Hours</h4>
+							<p><i class="fa fa-clock-o"></i> <span class="day">Weekdays:</span><span>9am to 6pm</span></p>
+							<p><i class="fa fa-clock-o"></i> <span class="day">Saturday:</span><span>9am to 1pm</span></p>
+							<p><i class="fa fa-clock-o"></i> <span class="day">Sunday:</span><span>Closed</span></p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<form name="sentMessage" id="contactForm" novalidate="">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Your Name *" id="name" required="" data-validation-required-message="Please enter your name.">
+										<p class="help-block text-danger"></p>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="email" class="form-control" placeholder="Your Email *" id="email" required="" data-validation-required-message="Please enter your email address.">
+										<p class="help-block text-danger"></p>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="form-control" placeholder="Your Message *" id="message" required="" data-validation-required-message="Please enter a message."></textarea>
+										<p class="help-block text-danger"></p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="row">
+								<div class="col-lg-12 text-center">
+									<div id="success"></div>
+									<button type="submit" class="btn">Send Message</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		<p id="back-top">
+			<a href="#top"><i class="fa fa-angle-up"></i></a>
+		</p>
+		<footer>
+			<div class="container text-center">
+				<p>Copyright &copy; E-Printing Syamsul Amri Bin Jamaludin B031710093</p>
+			</div>
+		</footer>
 
-              <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
-                <label for="inputPassword">Password</label>
-              </div>
+		<!-- Modal for portfolio item 1 -->
+		<div class="modal fade" id="Modal-1" tabindex="-1" role="dialog" aria-labelledby="Modal-label-1">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-1">Dean & Letter</h4>
+					</div>
+					<div class="modal-body">
+						<img src="imagesNantes/demo/portfolio-1.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Branding</span><span>Web Design</span></div>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
 
-              <br>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="submit">Sign in</button>
-              <br>
-              <a href="register.php">Create Your Account Here Now!!!</a>
-          
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+		<!-- Modal for portfolio item 2 -->
+		<div class="modal fade" id="Modal-2" tabindex="-1" role="dialog" aria-labelledby="Modal-label-2">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-2">Startup Framework</h4>
+					</div>
+					<div class="modal-body">
+						<img src="imagesNantes/demo/portfolio-2.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Branding</span><span>Web Design</span></div>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
 
+		<!-- Modal for portfolio item 3 -->
+		<div class="modal fade" id="Modal-3" tabindex="-1" role="dialog" aria-labelledby="Modal-label-3">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-3">Lamp & Velvet</h4>
+					</div>
+					<div class="modal-body">
+						<img src="imagesNantes/demo/portfolio-3.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Branding</span><span>Web Design</span></div>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
+		<!-- Modal for portfolio item 4 -->
+		<div class="modal fade" id="Modal-4" tabindex="-1" role="dialog" aria-labelledby="Modal-label-4">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-4">Smart Name</h4>
+					</div>
+					<div class="modal-body">
+						<img src="imagesNantes/demo/portfolio-4.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Branding</span><span>Web Design</span></div>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
 
-</body>
+		<!-- Modal for portfolio item 5 -->
+		<div class="modal fade" id="Modal-5" tabindex="-1" role="dialog" aria-labelledby="Modal-label-5">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-5">Fast People</h4>
+					</div>
+					<div class="modal-body">
+						<img src="imagesNantes/demo/portfolio-5.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Branding</span><span>Web Design</span></div>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Bootstrap core JavaScript
+			================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+		<script src="jsNantes/bootstrap.min.js"></script>
+		<script src="jsNantes/owl.carousel.min.js"></script>
+		<script src="jsNantes/cbpAnimatedHeader.js"></script>
+		<script src="jsNantes/jquery.appear.js"></script>
+		<script src="jsNantes/SmoothScroll.min.js"></script>
+		<script src="jsNantes/theme-scripts.js"></script>
+	</body>
+</html>
